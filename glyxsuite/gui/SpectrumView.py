3b1534716fbf24c2877b0541bdde3c58aeac074f
Markus Pioch <pioch@mpi-magdeburg.mpg.de>
2014-11-26 10:42:21 +0100

    working specrtrumview

--- glyxsuite/gui/SpectrumView.py
+++ glyxsuite/gui/SpectrumView.py
@@ -1,5 +1,6 @@
 import ttk 
 from Tkinter import * 
+import math
 
 class PeakTMP:
     
@@ -47,7 +48,7 @@ class SpectrumView(ttk.Frame):
         self.spectrumMaxInt = -1
         
         self.viewXMin = 0
-        self.viewXMax = 1000
+        self.viewXMax = -1
         self.viewYMin = 0
         self.viewYMax = -1
         self.dimensionScrollregion = (0,0,600,300) # Pixel dimensions of the spectrum
@@ -58,15 +59,10 @@ class SpectrumView(ttk.Frame):
         self.height = 300
         self.width = 600
         self.border = 50
-        
-        xscrollbar = Scrollbar(self, orient=HORIZONTAL)
-        xscrollbar.grid(row=1, column=0, sticky=E+W)
 
-        yscrollbar = Scrollbar(self)
-        yscrollbar.grid(row=0, column=1, sticky=N+S)
-        
-        self.spectrum = Canvas(self, width=600, height=300, scrollregion=(0, 0, 600, 300),xscrollcommand=xscrollbar.set,
-                yscrollcommand=yscrollbar.set) # check screen resolution
+        self.spectrum = Canvas(self, width=600, height=300) # check screen resolution
+        #self.spectrum = Canvas(self, width=600, height=300, scrollregion=(0, 0, 600, 300),xscrollcommand=xscrollbar.set,
+        #        yscrollcommand=yscrollbar.set) # check screen resolution                
         self.spectrum.grid(row=0, column=0, sticky=N+S+E+W)
         
         b1 = Button(self, text="save",command=self.save)
@@ -77,15 +73,10 @@ class SpectrumView(ttk.Frame):
         
         self.coord = StringVar()
         l = Label( self,textvariable=self.coord)
-        l.grid(row=3, column=1, sticky=N+S)
+        l.grid(row=3, column=0, sticky=N+S)
                 
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
-        
-
-        
-        xscrollbar.config(command=self.spectrum.xview)
-        yscrollbar.config(command=self.spectrum.yview)
         
         
         # add events for spectrum canvas
@@ -100,6 +91,10 @@ class SpectrumView(ttk.Frame):
         
         # link function
         self.model.funcPaintSpectrum = self.makeSpectrum
+        
+    def scrollX(self,event,args):
+        self.xscrollbar.set(event,args)
+        #self.spectrum.xview
      
     def eventMouseMotion(self,event):
         self.coord.set(str(round(self.convert_X_to_MZ(event.x),2))+"/"+str(round(self.convert_Y_to_Int(event.y),0)))
@@ -148,77 +143,131 @@ class SpectrumView(ttk.Frame):
             self.viewYMin = 0
             
         if self.viewXMax == -1:
-            self.viewXMax = self.spectrumMaxMZ
+            self.viewXMax = self.spectrumMaxMZ*1.1
         if self.viewYMax == -1:
-            self.viewYMax = self.spectrumMaxInt
+            self.viewYMax = self.spectrumMaxInt*1.1
         
         # calc slopes
-        print "foo",self.viewXMax,self.viewYMax
         self.slopeMZ = (self.width-2*self.border)/float(self.viewXMax-self.viewXMin)
         self.slopeInt = (self.height-2*self.border)/float(self.viewYMax-self.viewYMin)
-        
-        
-        # calculate dimensionScrollregion
-        self.dimensionScrollregion = [0,0]
-        self.dimensionScrollregion.append(int(self.slopeMZ*self.spectrumMaxMZ))
-        self.dimensionScrollregion.append(int(self.slopeInt*self.spectrumMaxInt))
-        print self.dimensionScrollregion
-        self.spectrum.config(scrollregion=self.dimensionScrollregion)
-        
-        
+
     def convert_MZ_to_X(self,MZ):
-        return self.border+self.slopeMZ*MZ
+        return self.border+self.slopeMZ*(MZ-self.viewXMin)
 
     def convert_Int_to_Y(self,Int):
-        return self.height-self.border-self.slopeInt*Int
+        return self.height-self.border-self.slopeInt*(Int-self.viewYMin)
 
     def convert_X_to_MZ(self,X):
         if self.model.spec == None:
             return X
-        return (X-self.border)/self.slopeMZ
+        return (X-self.border)/self.slopeMZ+self.viewXMin
 
     def convert_Y_to_Int(self,Y):
         if self.model.spec == None:
             return Y
-        return (self.height-self.border-Y)/self.slopeInt
+        return (self.height-self.border-Y)/self.slopeInt+self.viewYMin
 
-        
-    def makeSpectrum(self,spec):
+    def initSpectrum(self,spec):
+        print "init spectrum"
         if spec == None:
             return
         self.model.spec = spec
         self.setSpectrumRange(spec)
-        self.calcScales()
+        
+        self.makeSpectrum()
+        
+        
+    def makeSpectrum(self):
+        
+        self.calcScales() 
         self.spectrum.delete(ALL)
-        for peak in spec:
-            pMZ = self.convert_MZ_to_X(peak.getMZ())
-            pInt = self.convert_Int_to_Y(peak.getIntensity())
-            pInt0 = self.convert_Int_to_Y(0) # fix, zoom
+        for peak in self.model.spec:
+            mz = peak.getMZ()
+            intens = peak.getIntensity()
+            if mz < self.viewXMin or mz > self.viewXMax:
+                continue
+            if intens < self.viewYMin:
+                continue
+            if intens > self.viewYMax:
+                intens = self.viewYMax
+            pMZ = self.convert_MZ_to_X(mz)
+            pInt = self.convert_Int_to_Y(intens)
+            pInt0 = self.convert_Int_to_Y(self.viewYMin)
 
-            item = self.spectrum.create_line(pMZ,pInt0,pMZ,pInt,tags=("scale",))
+            item = self.spectrum.create_line(pMZ,pInt0,pMZ,pInt,tags=("peak",))
+        # create axis
+        self.spectrum.create_line(self.convert_MZ_to_X(self.viewXMin),
+                                    self.convert_Int_to_Y(self.viewYMin),
+                                    self.convert_MZ_to_X(self.viewXMax),
+                                    self.convert_Int_to_Y(self.viewYMin),
+                                    tags=("axis",))
+        self.spectrum.create_line(self.convert_MZ_to_X(self.viewXMin),
+                                    self.convert_Int_to_Y(self.viewYMin),
+                                    self.convert_MZ_to_X(self.viewXMin),
+                                    self.convert_Int_to_Y(self.viewYMax),
+                                    tags=("axis",))
+                                    
+        # search scale X
+        start,end,diff,exp = findScale(self.viewXMin,self.viewXMax,5.0)
+        print  "diff",diff,exp
+        while start < end:
+            if start > self.viewXMin and start < self.viewXMax:
+                x = self.convert_MZ_to_X(start)
+                y = self.convert_Int_to_Y(self.viewYMin)
+                self.spectrum.create_text(
+                    (x,y+5),text = shortNr(start,exp),anchor=N)
+                self.spectrum.create_line(x,y,x,y+4)
+            start += diff
+
+        # search scale Y
+        start,end,diff,exp = findScale(self.viewYMin,self.viewYMax,5.0)
+        while start < end:
+            if start > self.viewYMin and start < self.viewYMax:
+                x = self.convert_MZ_to_X(self.viewXMin)
+                y = self.convert_Int_to_Y(start)
+                self.spectrum.create_text(
+                    (x-5,y),text = shortNr(start,exp),anchor=E)
+                self.spectrum.create_line(x-4,y,x,y)
+            start += diff
             
 
     def zoom(self,x1,y1,x2,y2):
+        print "zoom",x1,y1,x2,y2
+        print"slope",self.slopeMZ,self.slopeInt
         if self.model.spec == None:
             return
+        xa = self.convert_X_to_MZ(x1)
+        xb = self.convert_X_to_MZ(x2)
         if x1 < x2:
-            self.viewXMin = self.convert_X_to_MZ(x1)
-            self.viewXMax = self.convert_X_to_MZ(x2)
+            self.viewXMin = xa
+            self.viewXMax = xb
         else:
-            self.viewXMin = self.convert_X_to_MZ(x2)
-            self.viewXMax = self.convert_X_to_MZ(x1)
+            self.viewXMin = xb
+            self.viewXMax = xa
+            
+        ya = self.convert_Y_to_Int(y1)
+        yb = self.convert_Y_to_Int(y2)
         if y1 < y2:
-            self.viewYMin = self.convert_Y_to_Int(y2)
-            self.viewYMax = self.convert_Y_to_Int(y1)
+            self.viewYMin = yb
+            self.viewYMax = ya
         else:
-            self.viewYMin = self.convert_Y_to_Int(y1)
-            self.viewYMax = self.convert_Y_to_Int(y2)
-        self.makeSpectrum(self.model.spec)
-        
-        # scroll to new area
-        self.spectrum.xview_moveto(self.viewXMin/self.spectrumMaxMZ) 
-        self.spectrum.yview_moveto(self.viewYMin/self.spectrumMaxInt) 
-        print "zoomed"
+            self.viewYMin = ya
+            self.viewYMax = yb
+
+        # check maximal parameters
+        if self.viewXMax > self.spectrumMaxMZ:
+            self.viewXMax = self.spectrumMaxMZ*1.1
+        if self.viewXMin < 0:
+            self.viewXMin = 0
+        if self.viewYMax > self.spectrumMaxInt:
+            self.viewYMax = self.spectrumMaxInt*1.1
+        if self.viewYMin < 0:
+            self.viewYMin = 0
+         
+        print "setX", self.viewXMin, self.viewXMax
+        print "setY", self.viewYMin, self.viewYMax
+        self.makeSpectrum()
+
         
     def save(self):
         f = file("out.txt","w")
@@ -237,5 +286,31 @@ class SpectrumView(ttk.Frame):
                 
         print "loaded"
         print len(spec)
-        self.makeSpectrum(spec)
+        self.viewXMin = 0
+        self.viewXMax = -1
+        self.viewYMin = 0
+        self.viewYMax = -1        
+        self.initSpectrum(spec)
+
+def shortNr(nr,exp):
+    # shorten nr if precision is necessary
+    if exp <= 0:
+        return round (nr,int(-exp+1))
+    return round(nr,0)
                 
+def findScale(start,end,NrScales):
+    diff = abs(end-start)/NrScales
+    exp = math.floor(math.log(diff)/math.log(10))
+    base = 10**exp
+    nr = diff/base
+    if nr < 0 or nr >= 10:
+        raise Exception("error in scaling")
+    # choose nearest scale of [1,2,2.5,5]
+    scales = [1,2,2.5,5]
+    sortNr = [(abs(s-nr),s) for s in scales]
+    sortNr.sort()
+    diff = sortNr[0][1]*base
+    
+    startAxis = math.floor(start/diff)*diff
+    endAxis = math.ceil(end/diff)*diff
+    return startAxis,endAxis,diff,exp
