8244bd37747b58bb9bafd9c40338a5c9d6757156
Markus Pioch <pioch@mpi-magdeburg.mpg.de>
2018-06-30 15:21:47 +0200

    new tools identification discriminator and reporting tool for the HGI data

---
+++ TOPP/HGIReporter.py
@@ -0,0 +1,208 @@
+# Tool for generating excel data
+
+import glyxtoolms
+import sys  
+
+def getSpectrum(exp,nativeId):
+    msmsSpec = None
+    msSpec = None
+    index = int(nativeId.split("=")[-1])
+    msmsSpec = exp[index]
+    assert msmsSpec.getNativeID() == nativeId
+    assert msmsSpec.getMSLevel() == 2
+
+    #for index,s in enumerate(exp):
+    #    if s.getNativeID() == nativeId:
+    #        assert s.getMSLevel() == 2
+    #        msmsSpec = s
+    #        break
+    # find precursor spectrum
+    while index > 0:
+        index -= 1
+        s = exp[index]
+        if s.getMSLevel() == 1:
+            msSpec = s
+            break
+    return msSpec,msmsSpec
+
+
+def generateData(exp,s,hit,fragTyp,comment):
+    
+    msSpec, msmsSpec = getSpectrum(exp,s.getNativeId())
+    if msmsSpec == None or msSpec == None:
+        return {}
+    assert msSpec.getMSLevel() == 1
+    assert msmsSpec.getMSLevel() == 2
+    
+    data = {}
+    data["File"] = "B"
+    data["MSScan"] = msSpec.getNativeID().split("=")[-1]
+    msid = ""
+    if fragTyp != "":
+        msid = msmsSpec.getNativeID().split("=")[-1]
+    data["MS/MSScan"] = msid
+    data["RT"] = round(msSpec.getRT()/60.0,2)
+    data["DissMethod"] = fragTyp
+    data["SpectralAnn"] = "glyXtoolMS"
+    evidence = "MS, MS/MS"
+    if fragTyp == "":
+        evidence = "MS"
+    data["Evidence"] = evidence
+    score = ""
+    if fragTyp == "EThcD":
+        score = round(hit.toolValues.get("etdScore2",0.0),2)
+    elif fragTyp == "HCD":
+        score = round(hit.toolValues.get("hcdScore2",0.0),2)
+    data["IdConv1"] = score
+    data["IdConv2"] = ""
+    data["IdConv3"] = ""
+    data["ObserverdMZ"] = round(hit.feature.getMZ(),4)
+    data["ObserverdCharge"] = int(hit.feature.getCharge())
+    data["off"] = "0" # check!!
+    z = hit.feature.getCharge()
+    data["MassGPObserved"] = round(hit.feature.getMZ()*z-(z-1)*glyxtoolms.masses.MASS["H+"],4)
+    data["MassGPTh"] = round(hit.peptide.mass+hit.glycan.mass+glyxtoolms.masses.MASS["H+"],4)
+    data["Delta"] = round(data["MassGPObserved"]-data["MassGPTh"],4)
+    data["ppm"] = round(data["Delta"]/data["MassGPTh"]*1E6,4)
+    data["sequence"] = hit.peptide.sequence
+    data["start"] = hit.peptide.start+1
+    data["sites"] = ", ".join([str(pos) for pos in sorted([pos+1 for pos,typ in hit.peptide.glycosylationSites])])
+    modifications = []
+    for m in hit.peptide.modifications:
+        pos = hit.peptide.start+m.position+2
+        mass = round(glyxtoolms.masses.PROTEINMODIFICATION[m.name]["mass"],4)
+        modifications.append((pos, str(mass)))
+    modifications = sorted(modifications)
+    data["modsRes"] = ", ".join([str(pos) for pos,mass in modifications])
+    data["modsMass"] = ", ".join([mass for pos,mass in modifications])
+    data["Hex"] = hit.glycan.sugar.get("HEX",0) if hit.glycan.sugar.get("HEX",0) > 0  else ""
+    data["HexNAc"] = hit.glycan.sugar.get("HEXNAC",0) if hit.glycan.sugar.get("HEXNAC",0) > 0  else ""
+    data["Fuc"] = hit.glycan.sugar.get("DHEX",0) if hit.glycan.sugar.get("DHEX",0) > 0  else ""
+    data["NeuAc"] = hit.glycan.sugar.get("NEUAC",0) if hit.glycan.sugar.get("NEUAC",0) > 0  else ""
+    data["NeuGc"] = hit.glycan.sugar.get("NEUGC",0) if hit.glycan.sugar.get("NEUGC",0) > 0  else ""
+    data["GlycanMass"] = round(hit.glycan.mass,4)
+    data["GlycanTyp"] = ""
+    protSplit = hit.peptide.proteinID.split("|")
+    data["ProtName"] = protSplit[2]
+    data["UniProtKB"] = protSplit[1]
+    data["Comment"] = comment
+    return data
+
+
+def handle_args(argv=None):
+    import argparse
+    usage = "\nFile IdentificationDiscriminator"
+    parser = argparse.ArgumentParser(description=usage)
+    parser.add_argument("--inMZML", dest="inMZML",help="MzML file containing both ETD and HCD spectra") 
+    parser.add_argument("--inETD", dest="inETD",help="ETD Analysis File, also containing the identifications") 
+    parser.add_argument("--inHCD", dest="inHCD",help="HCD Analysis File") 
+    parser.add_argument("--out", dest="outfile",help="Table containing the necessary spectra information")
+    
+    if not argv:
+        args = parser.parse_args(sys.argv[1:])
+    else:
+        args = parser.parse_args(argv)
+    return args
+
+def main(options):
+    
+    exp = glyxtoolms.lib.openOpenMSExperiment(options.inMZML)
+
+    print "reading in ETD Analysis file"
+    etdFile = glyxtoolms.io.GlyxXMLFile()
+    etdFile.readFromFile(options.inETD)
+
+    print "reading in HCD Analysis file"
+    hcdFile = glyxtoolms.io.GlyxXMLFile()
+    hcdFile.readFromFile(options.inHCD)
+
+    print "generate feature lookup"
+    featureLookup = {}
+    for feature in etdFile.features:
+        featureLookup[feature.id] = [feature,None]
+        
+    for feature in etdFile.features:
+        featureLookup[feature.id] = featureLookup.get(feature.id,[None,None])
+        featureLookup[feature.id][1] = feature
+
+    # set identification origin
+    identifications = etdFile.glycoModHits
+
+    line = []
+    line.append("File")
+    line.append("MSScan")
+    line.append("MS/MSScan")
+    line.append("RT")
+    line.append("DissMethod")
+    line.append("SpectralAnn")
+    line.append("Evidence")
+    line.append("IdConv1")
+    line.append("IdConv2")
+    line.append("IdConv3")
+    line.append("ObserverdMZ")
+    line.append("ObserverdCharge")
+    line.append("off")
+    line.append("MassGPObserved")
+    line.append("MassGPTh")
+    line.append("Delta")
+    line.append("ppm")
+    line.append("sequence")
+    line.append("start")
+    line.append("sites")
+    line.append("modsRes")
+    line.append("modsMass")
+    line.append("Hex")
+    line.append("HexNAc")
+    line.append("Fuc")
+    line.append("NeuAc")
+    line.append("NeuGc")
+    line.append("GlycanMass")
+    line.append("GlycanTyp")
+    line.append("ProtName")
+    line.append("UniProtKB")
+    line.append("Comment")
+
+    f = file(options.outfile,"w")
+
+    for hit in identifications:
+        if hit.status != "Accepted":
+            continue
+        print hit.tags
+        # generate comment
+
+        if "explainedByPeptideInference" in hit.tags:
+            comment = "glycopeptide infered from the existence of the same peptide and other glycans (MS only)"
+        elif "explainedByProteinInference" in hit.tags:
+            comment = "glycopeptide infered from the existence of other peptides with shared glycosylation site"
+        elif "explainedByETD" in hit.tags and "explainedByHCD" in hit.tags:
+            comment = "glycopeptide identified by HCD and ETD"
+        elif "explainedByETD" in hit.tags:
+            comment = "glycopeptide identified by ETD"
+        else:
+            comment = "glycopeptide identified by HCD"
+
+        if "explainedByETD" in hit.tags:
+            etdFeature = featureLookup[hit.feature.id][0]
+            for s in etdFeature.spectra:
+                data = generateData(exp,s,hit,"EThcD",comment)
+                f.write("\t".join([str(data.get(col,"")) for col in line]) +"\n")
+        if "explainedByHCD" in hit.tags:
+            hcdFeature = featureLookup[hit.feature.id][1]
+            for s in hcdFeature.spectra:
+                data = generateData(exp,s,hit,"HCD",comment)
+                f.write("\t".join([str(data.get(col,"")) for col in line]) +"\n")
+        if "explainedByPeptideInference" in hit.tags or "explainedByProteinInference" in hit.tags:
+            hcdFeature = featureLookup[hit.feature.id][0]
+            etdFeature = featureLookup[hit.feature.id][1]
+            for s in etdFeature.spectra+hcdFeature.spectra:
+                data = generateData(exp,s,hit,"",comment)
+                f.write("\t".join([str(data.get(col,"")) for col in line]) +"\n")
+                break
+    f.close()
+
+    print "done"
+    return
+
+if __name__ == "__main__":
+    options = handle_args()
+    main(options)
---
+++ TOPP/IdentificationDiscriminator.py
@@ -0,0 +1,234 @@
+# Tool for calculating peptide scores for HCD and ETD fragments
+
+import glyxtoolms
+import sys 
+
+class Feature(object):
+    
+    def __init__(self,featureId):
+        self.featureId = featureId
+        self.identifications = set()
+        self.chosenIdentification = None
+       
+        self.bestETDScore = None
+        self.bestHCDScore = None
+        self.explainedETD = None
+        self.explainedHCD = None
+    
+    def getBestScores(self):
+        self.bestETDScore = max([ident.etdScore for ident in self.identifications])
+        self.bestHCDScore = max([ident.hcdScore for ident in self.identifications])
+
+class Identification(object):
+    
+    def __init__(self,featureId, peptide, glycan,rt):
+        self.featureId = featureId
+        self.peptide = peptide
+        self.glycan = glycan
+        self.rt = rt
+        self.etdScore = None
+        self.hcdScore = None
+        self.key = "|".join((featureId,peptide,glycan))
+        self.feature = None
+        self.removed = 0 # shows at which point the identification was removed
+        self.valid = False
+        self.proteins = set()
+        self.intensity = 0
+        self.hits = set()
+        
+    def accept(self,iteration):
+        if self.removed > 0:
+            return False
+        self.removed = iteration
+        # reject all other identifications within the same feature
+        for ident in self.feature.identifications:
+            ident.reject(iteration)
+        self.valid = True
+        self.feature.chosenIdentification = self
+        return True
+
+    def reject(self,iteration):
+        if self.removed > 0:
+            return
+        self.removed = iteration
+        self.valid = False        
+
+def handle_args(argv=None):
+    import argparse
+    usage = "\nFile IdentificationDiscriminator"
+    parser = argparse.ArgumentParser(description=usage)
+    parser.add_argument("--in", dest="infile",help="File input Analysis file identifications scored on ETD and HCD .xml") 
+    parser.add_argument("--out", dest="outfile",help="File output Analysis file with accepted identifications and explained features")
+    parser.add_argument("--minScore", dest="minScore",help="minimum score  if identification should be accepted in the first round")
+    parser.add_argument("--minExplainedIntensity", dest="minExplainedIntensity",help="minimum explained fragment spectrum intensity if identification should be accepted in the first round")
+    if not argv:
+        args = parser.parse_args(sys.argv[1:])
+    else:
+        args = parser.parse_args(argv)
+    return args
+
+def main(options):
+    
+    print "parsing parameters"
+    minExplainedIntensity = float(options.minExplainedIntensity)
+    minScoreValue = float(options.minScore)
+    print "minExplainedIntensity: ", minExplainedIntensity
+    print "minScore: ", minScoreValue
+    
+    print "parsing input file"
+    glyML = glyxtoolms.io.GlyxXMLFile()
+    glyML.readFromFile(options.infile)
+    
+    
+    print "generating lookup data"
+    data = {}
+    for hit in glyML.glycoModHits:
+        key = "|".join((hit.feature.id,hit.peptide.toString(),hit.glycan.toString()))
+        if not key in data:
+            ident = Identification(hit.feature.id,hit.peptide.toString(),hit.glycan.toString(),hit.feature.getRT())
+            ident.intensity = hit.feature.intensity
+            ident.etdScore = hit.toolValues.get("etdScore2",0.0)
+            ident.hcdScore = hit.toolValues.get("hcdScore2",0.0)
+            ident.explainedETD = hit.toolValues.get("explainedETD",0.0)
+            ident.explainedHCD = hit.toolValues.get("explainedHCD",0.0)
+            data[key] = ident
+        ident = data[key]
+        ident.proteins.add(hit.peptide.proteinID)
+        ident.hits.add(hit)
+    
+    features = {}
+    peptides = {}
+    for ident in data.values():
+        if not ident.featureId in features:
+            feature = Feature(ident.featureId)
+            features[ident.featureId] = feature
+        else:
+            feature = features[ident.featureId]
+        ident.feature = feature
+        feature.identifications.add(ident)
+        peptides[ident.peptide] = peptides.get(ident.peptide,[]) + [ident]
+    
+    for feature in features.values():
+        feature.getBestScores()
+    
+    sortedIdentifications = sorted(data.values(),key=lambda ident:max((ident.etdScore,ident.hcdScore)),reverse=True)
+    
+    # identify best scored identifications
+    iteration = 0
+    identified = []
+    explainedInt = 1.0
+    minScore = 200
+    print "start finding best peptides"
+    while True:
+        iteration += 1
+        start = None
+        for ident in sortedIdentifications:
+            if ident.removed != 0:
+                continue
+            passes = False
+            for bestScore,explained,typ in [(ident.etdScore,ident.explainedETD,"ETD"),(ident.hcdScore,ident.explainedHCD,"HCD")]:
+                if bestScore >= minScore and explained >= explainedInt:
+                    passes = True
+                    break
+            if passes == True:
+                start = ident
+                break
+        if start == None:
+            if minScore < minScoreValue:
+                break
+            else:
+                explainedInt -= 0.1
+                if explainedInt <= minExplainedIntensity:
+                    explainedInt = 1.0
+                    minScore -= 1
+                continue
+        if start.accept(iteration) == True:
+            identified.append(start)
+    print "found ", len(identified), " good scoring peptides"
+    
+    # add tags
+    for ident in identified:
+        for bestScore,explained,typ in [(ident.etdScore,ident.explainedETD,"ETD"),(ident.hcdScore,ident.explainedHCD,"HCD")]:
+            if bestScore >= minScoreValue and explained >= minExplainedIntensity:
+                # add explanation tags
+                for hit in ident.hits:
+                    hit.tags.add("explainedBy"+typ)
+    
+    # accept all remaining identification of the same peptide
+    newidentified = []
+    for start in identified:
+        iteration += 1
+        for ident in peptides[start.peptide]:
+            if ident.accept(iteration) == True:
+                newidentified.append(ident)
+                # add explanation tags
+                for hit in ident.hits:
+                    hit.tags.add("explainedByPeptideInference")
+    identified += newidentified
+    print "found ", len(newidentified), " peptides with the same sequence but lesser scores"
+    
+    newidentified = []
+    # accept all remaining identifications of the same protein/glycosylationsite
+    for start in identified:
+        iteration += 1
+        # collect proteins and the already identified glycosylation sites
+        prots = {}
+        for hit in start.hits:
+            prot = hit.peptide.proteinID
+            sites = set([typ+str(pos) for pos,typ in hit.peptide.glycosylationSites])
+            prots[prot] = prots.get(prot,set()).union(sites)
+        
+        for ident in sortedIdentifications:
+            if ident.removed != 0:
+                continue
+            intersects = False
+            for hit in ident.hits:
+                prot = hit.peptide.proteinID
+                if not prot in prots:
+                    continue            
+                sites = set([typ+str(pos) for pos,typ in hit.peptide.glycosylationSites])
+                if len(prots[prot].intersection(sites)) > 0:
+                    intersects = True
+                    break
+            if intersects == True and ident.accept(iteration) == True:
+                newidentified.append(ident)
+                # add explanation tags
+                for hit in ident.hits:
+                    hit.tags.add("explainedByProteinInference")
+    identified += newidentified
+    print "found ", len(newidentified), " peptides with the same glycosylation site but other sequence (misse cleavage, protein modifications)"
+    
+    # accept and reject identifications
+    accepted = set()
+    rejected = set()
+    for ident in identified:
+        for ident2 in ident.feature.identifications:
+            rejected = rejected.union(ident2.hits) 
+        accepted = accepted.union(ident.hits)
+    
+    rejected = rejected.difference(accepted)
+    print "accepted hits", len(accepted)
+    print "rejected hits", len(rejected)
+    
+    print "identified", len(identified), " of ", len(glyML.glycoModHits), " within ", len(glyML.features), " features"
+    
+    print "writing file containing the accepted identifications"
+    
+    acceptedFeatures = set()
+    for hit in glyML.glycoModHits:
+        if hit in rejected:
+            hit.status = "Rejected"
+        elif hit in accepted:
+            hit.status = "Accepted"
+            hit.feature.status = "Accepted"
+            acceptedFeatures.add(hit.feature)
+        else:
+            hit.status = "Unknown"
+    glyML.writeToFile(options.outfile)
+
+    print "done"
+    return
+
+if __name__ == "__main__":
+    options = handle_args()
+    main(options)
